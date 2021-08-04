ca2134196b53bf6abf2aeb1e20f4b1984dba027d
wendi nurhermansah <wendii.2mia3@gmail.com>
2021-08-04 13:43:38 +0700

    apotek2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 // use Auth;
 use Illuminate\Http\Request;
+use App\Models\Data_barang;
+use App\Models\Jenis_barang;
+use App\Models\Obat_terjual;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -12,7 +17,22 @@ class DashboardController extends Controller
     }
 
     public function index(){
+        $hari_ini = Carbon::now();
+        $hari = Carbon::today();
+        $bulan = $hari_ini->month;
         
-        return view('Home.dashboard');
+        $jenis = Jenis_barang::count('id');
+        $barang = Data_barang::sum('jumlah_barang');
+
+        $today = Obat_terjual::select(DB::raw("SUM(jumlah_harga) as total"))
+        ->whereDate('created_at', $hari)
+        ->get();
+        // dd($today);
+
+        $month = Obat_terjual::select(DB::raw("SUM(jumlah_harga) as total"))
+        ->whereMonth('created_at', $bulan)
+        ->get();
+        // dd($barang);
+        return view('Home.dashboard', compact('jenis','barang','today','month'));
     }
 }

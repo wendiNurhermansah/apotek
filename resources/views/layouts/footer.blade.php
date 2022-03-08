812d16f966ebea0e13844ebb169c5cb74ce87b2f
Wendi Nurhermansah <nurhermansahwendi@gmail.com>
2022-03-08 15:44:41 +0700

    barang masuk dan keluar selesai

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
 <div class="sticky">
-        <div style="background-color: #01913cs" class=" navbar-expand navbar-dark  justify-content-between bd-navbar accent-3">
+        <div style="background-color: #01913c" class=" navbar-expand navbar-dark  justify-content-between bd-navbar accent-3">
             <div class="mt-2 mb-2">
                 <p style="color: white; text-align:center;">Copyright © ApotekAsyifa2022</p>
             </div>
